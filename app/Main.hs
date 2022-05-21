84c02491e67ef757556fdae058536f666071c20c
Amélia SZK <amelia.szk@protonmail.com>
2022-05-21 17:32:14 -0400

    stringifyPixels

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,29 @@
 {-# HLINT ignore "Use head" #-} -- compiler was whining at `argv !! 0`... -_-
 module Main where
 
+{- ARCHITECTURE
+1) Parse the command line arguments
+2) Decode input image and check that we can process it
+3) Compute the pairs of adjacencies
+    + Don't forget about the diagonals option
+4) Sort the pairs as defined in API
+5) Construct the output string
+6) Write output string to file
+
+Most of the runtime will be spent in:
+    3) Compute the pairs of adjacencies
+
+Most of the development time & code lines are expected to be spent in the rest.
+-}
+
 import System.Environment
 import Codec.Picture
 import Data.Either
+import Data.List (intercalate)
 
 main :: IO ()
 main = do
+    -- # 1) PARSE COMMAND LINE ARGUMENTS #
     putStrLn "Start of CoulAdj-Hs"
     argv <- getArgs
     let argc = length argv
@@ -18,6 +35,7 @@ main = do
     print (dontRelateDiagonals settings)
     print (imageFileArg settings)
     print (resultFileArg settings)
+    -- # 2) DECODE INPUT IMAGE #
     imgEither <- readImage (imageFileArg settings)
     let imgDyn = either error id imgEither
     let img = convertRGBA8 imgDyn -- TODO: Check & crash if img isn't RGB8 or RGBA8
@@ -25,14 +43,31 @@ main = do
     print (imageWidth img)
     putStr "imageHeight = "
     print (imageHeight img)
-    writeFile (resultFileArg settings) "Hi again! :D\nThis should have erased the previous content."
+    let testPixel = PixelRGBA8 10 220 230 249
+    let testPixelBlu = PixelRGBA8  10  20 230 252
+    let testPixelGre = PixelRGBA8  10 220  30 250
+    let testPixelCya = PixelRGBA8  10 220 230 248
+    let testPixelRed = PixelRGBA8 210  20  30 246
+    let testPixelMag = PixelRGBA8 210  20 230 244
+    let testPixelYel = PixelRGBA8 210 220  30 242
+    print testPixel
+    let allColours = [
+            (0, 0, testPixelBlu),
+            (0, 0, testPixelGre),
+            (0, 0, testPixelCya),
+            (0, 0, testPixelRed),
+            (0, 0, testPixelMag),
+            (0, 0, testPixelYel)
+            ]
+    writeFile (resultFileArg settings) (stringifyPixels allColours)
+    print "Goodbye"
+
 
 data ProgSettings = ProgSettings
     {   dontRelateDiagonals :: Bool,
         imageFileArg :: String,
         resultFileArg :: String    
     }
-
 
 parseArgs :: Int -> [String] -> ProgSettings
 parseArgs argc argv
@@ -46,3 +81,35 @@ parseArgs argc argv
     | otherwise = error ("BUG: This error should never have happened. This is the `otherwise` in parseArgs, "
         ++ "and the first guard checked that the number of arguments was 2 or 3, and then other guards"
         ++ "treated both the case for 2 and 3. Or should have, because obviously something went wrong.")
+
+
+computeAdjacencies :: Image PixelRGBA8 -> [(PixelRGBA8, PixelRGBA8)]
+computeAdjacencies = undefined
+
+sortAdjacencies :: [(PixelRGBA8, PixelRGBA8)] -> [(PixelRGBA8, PixelRGBA8)]
+sortAdjacencies = undefined
+
+stringify :: [(PixelRGBA8, PixelRGBA8)] -> String
+stringify = undefined
+
+rgbAlphaToTsv :: PixelRGBA8 -> String
+rgbAlphaToTsv (PixelRGBA8 r g b a) = intercalate "\t" (map show [r, g, b, a])
+
+-- For debug
+extractPixels :: Image PixelRGBA8 -> [PixelRGBA8]
+extractPixels = undefined
+
+-- For debug
+pixelsHeader :: String
+pixelsHeader = intercalate "\t" ["row", "col", "r", "g", "b", "a"]
+
+-- For debug
+stringifyPixels :: [(Int, Int, PixelRGBA8)] -> String
+stringifyPixels pixels = 
+    let 
+        flattened = [(row, col, r, g, b, a) | (row, col, PixelRGBA8 r g b a) <- pixels ]
+        toString = [ [show row, show col, show r, show g, show b, show a] | (row, col, r, g, b, a) <- flattened ]
+        addedTabulation = [ intercalate "\t" field | field <- toString]
+        allLines = pixelsHeader : addedTabulation
+        addedNewLines = unlines allLines
+    in addedNewLines
